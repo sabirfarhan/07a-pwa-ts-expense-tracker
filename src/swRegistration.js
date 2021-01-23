18c2c2ebbad553bfe7e785ca402eb1d6f8b04e3f
farhansabir <farhansabir50043@gmail.com>
2021-01-23 20:35:28 +0500

    modifiying swRegistration to resolve error

--- src/swRegistration.js
+++ src/swRegistration.js
@@ -2,7 +2,7 @@ export default function swRegistration() {
 
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/sw.js`).then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
