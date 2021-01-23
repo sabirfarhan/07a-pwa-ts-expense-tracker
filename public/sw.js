e6ba53c1e8ee1a49a45a2abdc39892ab25c92fe9
farhansabir <farhansabir50043@gmail.com>
2021-01-23 18:33:09 +0500

    commiting the initial project

---
+++ public/sw.js
@@ -0,0 +1,34 @@
+var cacheData = 'cache-v1';
+var urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/js/0.chunk.js',
+  '/static/js/main.chunk.js',
+  '/index.html'
+
+];
+
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(cacheData)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+    event.respondWith(
+      caches.match(event.request)
+        .then(function (response) {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+        )
+    )
+  }
+
+})
