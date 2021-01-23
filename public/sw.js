8fb2d89e3252cbff74dbba7ee2e629d8763dff90
farhansabir <farhansabir50043@gmail.com>
2021-01-23 20:57:37 +0500

    undo changes to previous one

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,11 @@
 var cacheData = 'cache-v1';
 var urlsToCache = [
-  
-    "/static/css/main.ee35176f.chunk.css",
-    "/static/js/main.7147c22d.chunk.js",
-    "/static/js/main.7147c22d.chunk.js.map",
-    "/static/js/runtime-main.c299a62f.js",
-    "/static/js/runtime-main.c299a62f.js.map",
-    "/static/js/2.68c515d9.chunk.js",
-    "/static/js/2.68c515d9.chunk.js.map",
-    "/static/js/3.e8c06914.chunk.js",
-    "/static/js/3.e8c06914.chunk.js.map",
-    "/index.html"
+
+  '/',
+  '/static/js/bundle.js',
+  '/static/js/0.chunk.js',
+  '/static/js/main.chunk.js',
+  '/index.html'
 
 ];
 
